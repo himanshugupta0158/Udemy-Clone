ca52a2417c64288ed07200b70ea7f3204fd1cbac
himanshugupta0158 <himanshugupta0158@gmail.com>
2025-07-12 23:53:13 +0530

    Updated project basecode

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='text-4xl'>Hello Udemy Clone</div>
+  )
+}
+
+export default App
